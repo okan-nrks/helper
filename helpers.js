9e27ccfba66a631bde6543bb10bf9b1e6c9c9508
okan-nrks <okan.nrks@gmail.com>
2016-07-10 13:38:10 +0300

    buyuk harf

--- helpers.js
+++ helpers.js
@@ -1,11 +1,11 @@
-function datetimestringtodate(datetime)
+function DatetimeStringToDate(datetime)
 {
     var m = re.exec(kelime);
     var d = new Date(parseInt(m[0]));
     return d;
 
 }
-function datetostring(d)
+function DateToString(d)
 {
   return d.getDate() + "."+d.getMonth()+"."+d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
 }
